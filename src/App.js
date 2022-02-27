8d9cf22c51477e49074ff4a6823767c9df15ed8d
kyuwon53 <lkw2v@naver.com>
2022-02-27 13:14:55 +0900

    [추가] 컴포넌트 삭제시 동작을 하도록 하라 return에 함수를 추가하면 컴포넌트가 삭제되면서 그 동작을 리턴한다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
-
+function Hello() {
+  useEffect(() => {
+    console.log("created :)");
+    return () => console.log("bye :(");
+  }, []);
+  return <h1>Hello</h1>;
+}
 function App() {
-  const [counter, setValue] = useState(0);
+  const [showing, setShowing] = useState(false);
   const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
+  const onClick = () => setShowing((prev) => !prev);
   const onChange = (event) => setKeyword(event.target.value);
   console.log("i run only once.");
   useEffect(() => {
@@ -13,22 +19,17 @@ function App() {
   useEffect(() => {
     console.log("I run when 'keyword' changes");
   }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword' & 'counter' changes");
-  }, [keyword, counter]);
   return (
     <div >
+      {showing ? <Hello /> : null}
       <input
         value={keyword}
         onChange={onChange}
         type="text"
         placeholder="Search here..."
       />
-      <h1 className={styles.title}>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      <h1 className={styles.title}></h1>
+      <button onClick={onClick}>{showing ? "Hide" : "show"}</button>
     </div>
   );
 }
