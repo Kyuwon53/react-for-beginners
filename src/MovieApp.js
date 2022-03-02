7fb911efcf4c1e95f7515c29785d868a81418501
kyuwon53 <lkw2v@naver.com>
2022-03-02 22:01:50 +0900

    [수정] 라우트 기본 페이지로 소스 이동

--- src/MovieApp.js
+++ src/MovieApp.js
@@ -1,39 +1,5 @@
-import { useEffect, useState } from "react";
-import Movie from "./Movie";
 function MovieApp() {
-  const [loading, setLoding] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoding(false);
-  }
-  useEffect(() => {
-    getMovies();
-    console.log({ movies })
-  }, []);
-  return (
-    <div>
-      {loading ?
-        <h1>Loading....</h1> :
-        (<div>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-        )}
-    </div>
-  );
+  return null;
 }
 
 export default MovieApp;
